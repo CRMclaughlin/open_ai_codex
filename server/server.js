60b4c306d725be67f4d02af12f60427cd2bd232e
CRMclaughlin <chrisraymusik@gmail.com>
2023-01-08 18:03:15 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+const PORT = process.env.PORT || 3500;
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX'
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt:`${prompt}`,
+            temperature:0,
+            max_tokens: 3000,
+            top_p:1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error || 'Something went Wrong')
+    }
+})
+
+app.listen(3500, () => console.log(`Server listening on port ${PORT}`));
